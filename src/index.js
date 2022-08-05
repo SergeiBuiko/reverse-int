667d50eb10b0a95074963120e974ea1080054bf6
Sergei Buiko <sergeibuiko1@gmail.com>
2022-08-05 16:28:14 +0300

    feat: add algorithm solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const y = (n.toString()).split('');
+    const z = y.reverse().join('')
+    
+    const lastSumbol = (z) => {
+    if (z[z.length -1] === '-') {return z.slice(0, z.length -1)}
+    return z
+    }
+
+    return lastSumbol(z)
 }
